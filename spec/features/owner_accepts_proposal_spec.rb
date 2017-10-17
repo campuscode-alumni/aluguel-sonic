cdca2dbee43eaf2c85f078591e627956e1f2ca9c
Nailson e Christian <nailsongomes@gmail.com>
2017-10-17 20:04:27 -0200

    teste de aceitar proposta

--- spec/features/owner_accepts_proposal_spec.rb
+++ spec/features/owner_accepts_proposal_spec.rb
@@ -5,30 +5,36 @@ feature 'owner accepts proposal' do
 
     proposal1 = Proposal.create(start_date: '16/10/2017',
                                 end_date: '19/10/2017',
-                                total_amount: 'R$600,00',
+                                total_amount: 600,
                                 total_guets: 5,
-                                status: 'Pendente')
+                                status: 'pending',
+                                property: property)
 
     proposal2 = Proposal.create(start_date: '16/10/2017',
                                 end_date: '19/10/2017',
-                                total_amount: 'R$600,00',
+                                total_amount: 600,
                                 total_guets: 10,
-                                status: 'Pendente')
+                                status: 'pending',
+                                property: property)
 
     proposal3 = Proposal.create(start_date: '16/10/2017',
                                 end_date: '19/10/2017',
-                                total_amount: 'R$600,00',
+                                total_amount: 600,
                                 total_guets: 4,
-                                status: 'Pendente')
+                                status: 'pending',
+                                property: property)
 
-    visit property_proposals_path proposal2
-
-    expect(page).to have_css('dd', text: proposal2.start_date)
-    expect(page).to have_css('dd', text: proposal2.end_date)
-    expect(page).to have_css('dd', text: proposal2.total_amount)
-    expect(page).to have_css('dd', text: proposal2.total_guets)
-    expect(page).to have_css('dd', text: proposal2.status)
-
+    visit property_proposals_path(proposal2)
     click_on 'Aceitar proposta'
+
+    expect(page).to have_content('Proposta aceita')
+    expect(page).to have_css('h2', text: 'Detalhes da proposta aceita')
+    expect(page).to have_css('dd', text: proposal2.property.name)
+    expect(page).to have_css('dd', text: '16/10/2017')
+    expect(page).to have_css('dd', text: '19/10/2017')
+    expect(page).to have_css('dd', text: 600)
+    expect(page).to have_css('dd', text: 5)
+    expect(page).to have_css('dd', text: 'accepts')
+
   end
 end
